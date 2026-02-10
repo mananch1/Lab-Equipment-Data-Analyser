f11ecdbf2e1d8c744bd1d1d6d07d720ef49668e2
unknown <mananchahal01@gmail.com>
2026-02-10 20:33:51 +0530

    Fixed deployment config

--- frontend-desktop/main.py
+++ frontend-desktop/main.py
@@ -8,7 +8,9 @@ from matplotlib.backends.backend_qt5agg import FigureCanvasQTAgg as FigureCanvas
 from matplotlib.figure import Figure
 import matplotlib.pyplot as plt
 
-API_URL = "http://127.0.0.1:8000/api"
+# PROD URL
+API_URL = "https://lab-equipment-data-analyser-backend.onrender.com/api"
+# API_URL = "http://127.0.0.1:8000/api" # LOCAL DEV
 
 class MplCanvas(FigureCanvas):
     def __init__(self, parent=None, width=5, height=4, dpi=100):
